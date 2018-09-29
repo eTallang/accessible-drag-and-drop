a3f2b7cf7b5835ddd9eb76fbd7bdaaaabaa14a5f
Erik Tallang <e.tallang@gmail.com>
2018-09-30 00:36:47 +0200

    feat(favorite-food): remove a11y module

--- src/app/favorite-food/favorite-food.module.ts
+++ src/app/favorite-food/favorite-food.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { A11yModule } from '@angular/cdk/a11y';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { FavoriteFoodComponent } from './favorite-food.component';
 
 @NgModule({
-  imports: [CommonModule, A11yModule, DragDropModule],
+  imports: [CommonModule, DragDropModule],
   declarations: [FavoriteFoodComponent],
   exports: [FavoriteFoodComponent]
 })
