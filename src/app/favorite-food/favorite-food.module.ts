bf1a1bd059ea07570e86cce4d0c68aee11f3e045
Erik Tallang <e.tallang@gmail.com>
2018-10-04 20:44:25 +0200

    feat(favorite-food): use slide toggle

--- src/app/favorite-food/favorite-food.module.ts
+++ src/app/favorite-food/favorite-food.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { A11yModule } from '@angular/cdk/a11y';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { MatCheckboxModule } from '@angular/material';
+import { MatSlideToggleModule } from '@angular/material';
 
 import { FavoriteFoodComponent } from './favorite-food.component';
 import { ListItemDirective } from './list-item.directive';
 
 @NgModule({
-  imports: [CommonModule, A11yModule, DragDropModule, MatCheckboxModule],
+  imports: [CommonModule, A11yModule, DragDropModule, MatSlideToggleModule],
   declarations: [FavoriteFoodComponent, ListItemDirective],
   exports: [FavoriteFoodComponent]
 })
